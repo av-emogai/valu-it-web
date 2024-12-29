6686d70376459080c7f9835ee94bd717c15b9eb4
Av-EmoGAI <157115886+av-emogai@users.noreply.github.com>
2024-12-29 00:01:01 -0500

    Fix for Jquery not found in some places.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import compression from "vite-plugin-compression";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),compression()],
+  plugins: [react(),compression(),
+    inject({
+      $: "jquery", // Inject jQuery as $
+      jQuery: "jquery", // Inject jQuery as jQuery
+    }),
+  ],
   //change port for production
   preview: {
     port: 3001,
